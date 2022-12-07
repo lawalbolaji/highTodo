3a3cb329893d14da7246772194f1b549628b54ee
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2022-12-07 15:28:06 +0100

    Add crud for todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,11 @@
 import React, { Component } from "react";
 import { Clock } from "./components/Clock";
 import { useTimer } from "./components/Timer";
-import { TodosComponent } from "./components/Todo/Todos";
-
-const mockTodos = [
-  {
-    id: 1,
-    description: "eat cake",
-    createdAt: new Date().toDateString(),
-  },
-  {
-    id: 2,
-    description: "eat cake",
-    createdAt: new Date("2022").toDateString(),
-  },
-  {
-    id: 3,
-    description: "eat cake",
-    createdAt: new Date("2021").toDateString(),
-  },
-  {
-    id: 4,
-    description: "eat cake",
-    createdAt: new Date("2020").toDateString(),
-  },
-  {
-    id: 5,
-    description: "eat cake",
-    createdAt: new Date("2019").toDateString(),
-  },
-];
+import { TodosComponent } from "./components/Todo";
 
 export function App() {
   return (
-    <div className="w-full flex flex-wra min-h-screen">
+    <div className="w-full flex flex-wrap min-h-screen">
       <div className="w-7/12 relative">
         {/* set font size, width etc. */}
         <div className="table absolute top-0 right-0 h-full w-full">
@@ -43,7 +15,7 @@ export function App() {
 
       <div className="w-5/12 relative">
         <div className="table absolute top-0 right-0 h-full w-full">
-          <TodosComponent todos={mockTodos} />
+          <TodosComponent />
         </div>
       </div>
     </div>
--- src/components/Todo/Todos.tsx
+++ src/components/Todo/Todos.tsx
@@ -1,34 +1,136 @@
-type TodosPropsType = {
-  todos: TodoType[];
-};
+import React from "react";
+
+type TodosPropsType = {};
 
 type TodoType = {
   id: number;
   description: string;
   createdAt: string;
+  isChecked: boolean;
 };
 
-export const TodosComponent = ({ todos }: TodosPropsType) => {
+type TodoCreateType = Omit<TodoType, "id" | "isChecked" | "createdAt">;
+
+const useTodoItems = () => {
+  const mockTodos = [
+    {
+      id: 1,
+      description: "eat cake",
+      createdAt: new Date().toDateString(),
+      isChecked: false,
+    },
+    {
+      id: 2,
+      description: "eat cake",
+      createdAt: new Date("2022").toDateString(),
+      isChecked: false,
+    },
+    {
+      id: 3,
+      description: "eat cake",
+      createdAt: new Date("2021").toDateString(),
+      isChecked: false,
+    },
+    {
+      id: 4,
+      description: "eat cake",
+      createdAt: new Date("2020").toDateString(),
+      isChecked: false,
+    },
+    {
+      id: 5,
+      description: "eat cake",
+      createdAt: new Date("2019").toDateString(),
+      isChecked: false,
+    },
+  ];
+
+  const [todoItems, setTodoItems] = React.useState<TodoType[]>(mockTodos);
+  const updateTodoItem = <T, K extends keyof T>(todoId: number, field: K, value: T[K]) => {
+    setTodoItems((todoItems) => {
+      return [...todoItems.filter((todo) => todo.id !== todoId), { ...todoItems.find((todoItem) => todoItem.id === todoId)!, id: todoId, [field]: value }].sort((a, b) => a.id - b.id);
+    });
+  };
+
+  const addNewTodoItem = (newTodo: TodoCreateType) => {
+    setTodoItems((todoItems) => {
+      return [...todoItems, { ...newTodo, id: todoItems[todoItems.length - 1].id + 1, createdAt: new Date().toDateString(), isChecked: false }];
+    });
+  };
+
+  return { todoItems, updateTodoItem, addNewTodoItem };
+};
+
+export const TodosComponent = ({}: TodosPropsType) => {
+  const { todoItems, updateTodoItem, addNewTodoItem } = useTodoItems();
+  const [newTodo, setNewTodo] = React.useState<string>("");
+
+  const handleCreateNewTodo = (e?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    addNewTodoItem({ description: newTodo });
+    setNewTodo("");
+  };
+
+  const handleNewTodoChange = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") handleCreateNewTodo();
+  };
+
   return (
     <div className="table-cell align-middle pr-8">
       <div className="border-2 rounded-md border-gray-200 dark:border-gray-600">
         <div className="p-4">
           <p className="px-2 py-3 text-xl font-bold">🪴 Essential Todo's</p>
           <ul>
-            {todos.map((todo: TodoType) => (
-              <li key={todo.id} className="w-full rounded-t-lg">
-                <div className="flex items-center pl-3">
-                  <input id="vue-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                  <label htmlFor="vue-checkbox" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">
-                    {todo.description}
-                  </label>
-                </div>
-              </li>
-            ))}
+            {todoItems
+              .filter((todoItem) => !todoItem.isChecked)
+              .map((todo: TodoType) => (
+                <li key={todo.id} className="w-full rounded-t-lg">
+                  <div className="flex items-center pl-3">
+                    <input
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateTodoItem<TodoType, "isChecked">(todo.id, "isChecked", e.currentTarget.checked)}
+                      checked={todo.isChecked}
+                      type="checkbox"
+                      className="w-6 h-6 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                    />
+                    <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateTodoItem<TodoType, "description">(todo.id, "description", e.currentTarget.value)} type="text" className="mx-2 border-0 focus:ring-0 w-full" value={todo.description} />
+                  </div>
+                </li>
+              ))}
           </ul>
 
-          {/* <input type="text" placeholder="enter todo item here" />
-          <button>add todo</button> */}
+          {/* new todo Item */}
+          <div className="w-full flex my-4">
+            <button className="px-3 hover:ring-blue-500 hover:ring-1 hover:bg-gray-100 hover:rounded" onClick={handleCreateNewTodo}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="w-6 h-full">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+            </button>
+
+            <div className="w-full">
+              <input value={newTodo} onKeyDown={handleNewTodoChange} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.currentTarget.value)} type="text" placeholder="add todo here" className="border-0 focus:ring-0 w-full" />
+            </div>
+          </div>
+
+          <hr />
+          {/* completed todo items */}
+          <div className="mt-4">
+            <ul>
+              {todoItems
+                .filter((todoItem) => todoItem.isChecked)
+                .map((todo: TodoType) => (
+                  <li key={todo.id} className="w-full rounded-t-lg">
+                    <div className="flex items-center pl-3">
+                      <input
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateTodoItem<TodoType, "isChecked">(todo.id, "isChecked", e.currentTarget.checked)}
+                        checked={todo.isChecked}
+                        type="checkbox"
+                        className="w-6 h-6 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                      />
+                      <input disabled type="text" className="mx-2 border-0 focus:ring-0 w-full italic line-through text-neutral-800" value={todo.description} />
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
